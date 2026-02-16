753cb8975a2d97d2294761f49e5146041b59f46f
nic-kup <niclas@kupper.co>
2026-02-16 14:23:12 +0000

    Implemented Hooks for ToyModel

--- experiments/dist_stack.py
+++ experiments/dist_stack.py
@@ -11,6 +11,12 @@ import plotly.express as px
 gen = torch.Generator("cpu")
 gen.manual_seed(2)
 
+list_of_Ws = []
+
+
+def my_hook(hook_data):
+    list_of_Ws.append(hook_data["ae"].W.detach().numpy().copy())
+
 
 dist = DistributionStack(
     [SparseUniform(3, 0.5, generator=gen) for i in range(3)], "single"
@@ -30,15 +36,18 @@ importances = torch.tensor([0.99**i for i in range(dist.n_features)])
 # %%
 ae = TiedLinearRelu(dist.n_features, n_hidden, generator=gen)
 tm = ToyModel(dist, ae, importances=importances)
-losses = tm.fit(40_000, verbose=False)
+losses = tm.fit(40_000, verbose=False, hooks=[my_hook], hook_freq=1000)
 
 # %%
 px.line(losses)
 
 # %%
-emb_mat = ae.W.detach().numpy()
-print(emb_mat.shape)
+i = 0
+# %%
+print(i)
+emb_mat = list_of_Ws[i]
+i += 1
 
 fig = px.scatter(x=emb_mat[0], y=emb_mat[1], color=list(range(dist.n_features)))
 fig.update_traces(marker={"size": 15})
-# %%
+
