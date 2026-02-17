94ffa8c6fd765b95ec92045df793f51cfbe54e65
nic-kup <niclas@kupper.co>
2026-02-17 10:25:52 +0000

    exchanged @cached_property for @property in toy model

--- experiments/dist_stack.py
+++ experiments/dist_stack.py
@@ -1,14 +1,14 @@
 """Example with Distribution Stack. See nice hierarchical structure in embedding matrix."""
 
 # %%
-from occhio.distributions import SparseUniform, DistributionStack, CorrelatedPairs
+from occhio.distributions import SparseUniform, DistributionStack
 from occhio.autoencoder import TiedLinearRelu
 from occhio.toy_model import ToyModel
 import torch
-import plotly.express as px
 from occhio.visualization import plot_dynamic_scatter
 
 # %%
+DEVICE = "cpu"
 gen = torch.Generator("cpu")
 gen.manual_seed(2)
 
@@ -17,16 +17,18 @@ def my_hook(hook_data):
     return hook_data["epoch"], hook_data["tm"].ae.W.detach().numpy().copy()
 
 
+# # Another hook here!
+# def my_hook(hook_data):
+#     return hook_data["epoch"], torch.stack(
+#         [hook_data["tm"].feature_dimensionalities, hook_data["tm"].total_feature_interferences]
+#     )
+
+
 dist = DistributionStack(
-    [SparseUniform(3, 0.5, generator=gen) for i in range(3)], "single"
+    [SparseUniform(3, 0.5, generator=gen, device=DEVICE) for i in range(3)],
+    "single",
+    device=DEVICE,
 )
-# dist = DistributionStack(
-#     [
-#         CorrelatedPairs(4, p_active=0.5, p_individual=0.7, generator=gen)
-#         for i in range(2)
-#     ],
-#     "single",
-# )
 
 
 n_hidden = 2
@@ -34,13 +36,14 @@ importances = torch.tensor([0.95**i for i in range(dist.n_features)])
 
 # %%
 ae = TiedLinearRelu(dist.n_features, n_hidden, generator=gen)
-tm = ToyModel(dist, ae, importances=importances)
-losses, hook_returns = tm.fit(40_000, verbose=False, hooks=[my_hook], hook_freq=500)
+tm = ToyModel(dist, ae, importances=importances, generator=gen, device=DEVICE)
+losses, hook_returns = tm.fit(
+    40_000, batch_size=256, verbose=False, hooks=[my_hook], hook_freq=1000
+)
 
-# %%
-px.line(losses)
 
 # %%
 # Interactive version with slider to explore embeddings at different epochs
 plot_dynamic_scatter(losses, hook_returns[0])
+
 # %%
--- src/occhio/toy_model.py
+++ src/occhio/toy_model.py
@@ -1,4 +1,3 @@
-from functools import cached_property
 from typing import Any, Optional, Callable
 
 import torch
@@ -92,22 +91,22 @@ class ToyModel:
 
         raise AttributeError(f"'{type(self).__name__}' has no attribute '{name}'")
 
-    @cached_property
+    @property
     @torch.no_grad()
     def froebenius_norm_squared(self):
         return torch.linalg.norm(self.W, ord="fro") ** 2
 
-    @cached_property
+    @property
     @torch.no_grad()
     def hidden_dimensions_per_embedded_features(self) -> Any:
         return self.ae.n_hidden / self.froebenius_norm_squared
 
-    @cached_property
+    @property
     @torch.no_grad()
     def embedded_features_per_hidden_dimensions(self) -> Any:
         return self.froebenius_norm_squared / self.ae.n_hidden
 
-    @cached_property
+    @property
     @torch.no_grad()
     def feature_dimensionalities(self):
         return (
@@ -115,48 +114,48 @@ class ToyModel:
             / self.total_feature_interferences_including_self
         )
 
-    @cached_property
+    @property
     @torch.no_grad()
     def mean_feature_dimensionalities(self):
         return self.feature_dimensionalities.mean()
 
-    @cached_property
+    @property
     @torch.no_grad()
     def total_feature_dimensionalities_per_hidden_dimension(self):
         return self.feature_dimensionalities.sum() / self.ae.n_hidden
 
-    @cached_property
+    @property
     @torch.no_grad()
     def W(self) -> Tensor:
         return self.get_one_hot_embeddings().T
 
-    @cached_property
+    @property
     @torch.no_grad()
     def W_normalized_features(self) -> Tensor:
         return F.normalize(self.W, dim=0)
 
-    @cached_property
+    @property
     @torch.no_grad()
     def feature_norms(self) -> Tensor:
         return torch.linalg.vector_norm(self.W, dim=0)
 
-    @cached_property
+    @property
     @torch.no_grad()
     def feature_representations(self) -> Tensor:
         return (self.W**2).sum(dim=0)
 
-    @cached_property
+    @property
     @torch.no_grad()
     def interferences(self) -> Tensor:
         return (self.W_normalized_features.T @ self.W) ** 2
 
-    @cached_property
+    @property
     @torch.no_grad()
     def total_feature_interferences(self) -> Tensor:
         interferences = self.interferences.clone()
         return interferences.fill_diagonal_(0).sum(dim=1)
 
-    @cached_property
+    @property
     @torch.no_grad()
     def total_feature_interferences_including_self(self) -> Tensor:
         return self.interferences.sum(dim=1)
